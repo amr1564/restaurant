baef7c616a7a722f5c1038176d64507f1ac5b637
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-03-23 16:00:23 -0400

    Add navigation bar

--- src/page-load.js
+++ src/page-load.js
@@ -1,15 +1,37 @@
 const pageLoad = (() => {
     const content = document.getElementById('content');
     
-    const header = document.createElement('div');
-    header.id = 'header';
-    header.innerHTML = 'Cafe Odin';
-    content.appendChild(header);
+    const headerContainer = document.createElement('div');
+    headerContainer.id = 'header-container';
+    content.appendChild(headerContainer);
 
-    const subHeader = document.createElement('div');
-    subHeader.id = 'sub-header';
-    subHeader.innerHTML = 'Fine dining for software developers';
-    content.appendChild(subHeader);
+    const home = document.createElement('div');
+    home.classList.add('nav');
+    home.id = 'home';
+    home.innerHTML = 'Home';
+    headerContainer.appendChild(home);
+
+    const menu = document.createElement('div');
+    menu.classList.add('nav');
+    menu.id = 'menu';
+    menu.innerHTML = 'Menu';
+    headerContainer.appendChild(menu);
+    
+    const contact = document.createElement('div');
+    contact.classList.add('nav');
+    contact.id = 'contact';
+    contact.innerHTML = 'Contact';
+    headerContainer.appendChild(contact);
+
+    const title = document.createElement('div');
+    title.id = 'title';
+    title.innerHTML = 'Cafe Odin';
+    content.appendChild(title);
+
+    const subTitle = document.createElement('div');
+    subTitle.id = 'sub-title';
+    subTitle.innerHTML = 'Fine dining for software developers';
+    content.appendChild(subTitle);
 
 })()
 
